2cfd8fc6d1bc48612e1007da7a6ca4f93792b8a2
Viniciu Cristian <git.viniciu@gmail.com>
2022-06-10 20:13:33 -0300

    first commit for deploy

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  url: string = '';
   constructor() {}
+
+  requestDownloadMusic() {
+    document.location.assign(`https://yt-download-sound.herokuapp.com/download?url=${this.url}`)
+  }
 
 }
